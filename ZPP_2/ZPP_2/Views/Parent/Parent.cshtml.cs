d3097efbbd85e94e78bb32d84517a76db8c277bf
JacobJanik <132952597+JacobJanik@users.noreply.github.com>
2024-09-03 22:02:28 +0200

    Add files via upload
    dodanie widoku dla rodzica

---
+++ ZPP_2/ZPP_2/Views/Parent/Parent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zpp_aplikacja.Pages.View.Parent
+{
+    public class ParentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ZPP_2/ZPP_2/Views/Parent/Tasks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zpp_aplikacja.Pages.View.Parent
+{
+    public class TaskModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
