ba4e923ba4ffc87dec2c6d1daa9b6943bc2a5259
JacobJanik <132952597+JacobJanik@users.noreply.github.com>
2024-09-03 22:03:40 +0200

    Update and rename ErrorViewModel.cs to Reward.cs
    zmiana error na reward

---
+++ ZPP_2/ZPP_2/Models/Reward.cs
@@ -0,0 +1,10 @@
+namespace zpp_aplikacja.Pages.Models
+{
+    public class Reward
+    {
+        public int RewardId { get; set; } 
+        public string Name { get; set; }
+        public int PointsRequired { get; set; }
+        public string Description { get; set; }
+    }
+}
