d71f5a47cfebe7e0ab9fdc755fbda32dce437b9c
JacobJanik <132952597+JacobJanik@users.noreply.github.com>
2024-09-03 22:01:56 +0200

    Add files via upload
    dodanie widoku dla dziecka

---
+++ ZPP_2/ZPP_2/Views/Child/Rewards.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zpp_aplikacja.Pages.View.Child
+{
+    public class RewardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
