740d479b8f1ade62920744295820126c4ad76331
JacobJanik <132952597+JacobJanik@users.noreply.github.com>
2024-09-03 21:56:42 +0200

    Update ChildController.cs
    naprawa kontrolera child

--- ZPP_2/ZPP_2/Controllers/ChildController.cs
+++ ZPP_2/ZPP_2/Controllers/ChildController.cs
@@ -1,73 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ZPP_2.Models;
-using ZPP_2.Repository;
+using Microsoft.AspNetCore.Mvc;
+using zpp_aplikacja.Pages.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 
-namespace ZPP_2.Controllers
+namespace YourProjectName.Controllers
 {
     public class ChildController : Controller
     {
-        private readonly ITodoRepository _todoRepository;
+        private string UsersFilePath = "Data/users.json";
+        private string TasksFilePath = "Data/tasks.json";
 
-        public ChildController(ITodoRepository todoRepository)
-        {
-            _todoRepository = todoRepository;
-        }
-
-        [HttpGet]
         public IActionResult Index()
         {
-            var model = _todoRepository.GetAll();
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult AddTodo()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult AddTodo(Todo model)
-        {
-            if (ModelState.IsValid)
+            // Sprawdź, czy użytkownik jest zalogowany i czy ma rolę dziecka
+            if (HttpContext.Session.GetString("UserRole") != UserRole.Child.ToString())
             {
-                _todoRepository.Insert(model);
-                _todoRepository.Save();
-                return RedirectToAction("Index", "Child");
+                return RedirectToAction("Index", "Parent");
             }
-            return View();
+
+            // Pobierz dane użytkownika z sesji
+            var userId = HttpContext.Session.GetInt32("UserId");
+            var user = GetUserById(userId.Value);
+
+            // Pobierz zadania z pliku JSON
+            var yourClassInstance = new Tasks();
+            var tasks = yourClassInstance.GetTaskById();
+
+            // Przekaż dane do widoku
+            ViewBag.Points = user.Points;
+            return View(tasks);
         }
-        [HttpGet]
-        public IActionResult EditTodo(int id)
+        private List<Task> GetTasksFromJson()
         {
-            Todo model = _todoRepository.GetById(id);
-            return View(model);
-        }
-        [HttpPost]
-        public IActionResult EditTodo(Todo model)
-        {
-            if (ModelState.IsValid)
+            if (System.IO.File.Exists(TasksFilePath))
             {
-                _todoRepository.Update(model);
-                _todoRepository.Save();
-                return RedirectToAction("Index", "Child");
+                string jsonString = System.IO.File.ReadAllText(TasksFilePath);
+                return JsonSerializer.Deserialize<List<Task>>(jsonString);
             }
             else
             {
-                return View(model);
+                return new List<Task>();
             }
         }
-        [HttpGet]
-        public IActionResult DeleteTodo(int id)
+
+        public IActionResult Create()
         {
-            Todo model = _todoRepository.GetById(id);
-            return View(model);
+            return View();
         }
-        [HttpPost]
-        public IActionResult Delete(int id)
+
+        private List<User> GetUsersFromJson()
         {
-            _todoRepository.Delete(id);
-            _todoRepository.Save();
-            return RedirectToAction("Index", "Child");
+            if (System.IO.File.Exists(UsersFilePath))
+            {
+                string jsonString = System.IO.File.ReadAllText(UsersFilePath);
+                return JsonSerializer.Deserialize<List<User>>(jsonString);
+            }
+            else
+            {
+                return new List<User>();
+            }
+        }
+
+        private void AddUserToJson(User user)
+        {
+            var users = GetUsersFromJson();
+            users.Add(user);
+            SaveUsersToJson(users);
+        }
+
+        private void SaveUsersToJson(List<User> users)
+        {
+            string jsonString = JsonSerializer.Serialize(users);
+            System.IO.File.WriteAllText(UsersFilePath, jsonString);
+        }
+
+        private User GetUserById(int userId)
+        {
+            var users = GetUsersFromJson();
+            return users.FirstOrDefault(u => u.Id == userId);
+        }
+
+        private void UpdateUsersFromJson(User user)
+        {
+            var users = GetUsersFromJson();
+            var existingUser = users.FirstOrDefault(u => u.Id == user.Id);
+
+            if (existingUser != null)
+            {
+                // Zaktualizuj dane użytkownika
+                existingUser.FirstName = user.FirstName;
+                existingUser.LastName = user.LastName;
+                existingUser.Email = user.Email;
+
+                SaveUsersToJson(users);
+            }
+        }
+        private void DeleteUserFromJson(int userId)
+        {
+            var users = GetUsersFromJson();
+            var userToRemove = users.FirstOrDefault(u => u.Id == userId);
+
+            if (userToRemove != null)
+            {
+                users.Remove(userToRemove);
+                SaveUsersToJson(users);
+            }
         }
     }
 }
