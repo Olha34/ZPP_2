422357093a8c71264d9697c8f62ca7f6d03f2804
JacobJanik <132952597+JacobJanik@users.noreply.github.com>
2024-09-03 22:05:15 +0200

    Add files via upload
    Dodanie modelu użytkownika

---
+++ ZPP_2/ZPP_2/Models/User.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace zpp_aplikacja.Pages.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } // Identyfikator użytkownika
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public int Points { get; set; } // Punkty użytkownika
+
+        public User()
+        {
+            Role = UserRole.Child; // Domyślnie ustawiamy rolę na dziecko
+            Points = 0; // Początkowa liczba punktów
+        }
+
+        public bool IsParent()
+        {
+            return Role == UserRole.Parent;
+        }
+
+        public void AddPoints(int points)
+        {
+            Points += points;
+        }
+    }
+
+    public enum UserRole
+    {
+        Child,
+        Parent
+    }
+}
