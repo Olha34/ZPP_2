f86300289aa8d3dc82ca7b957b2b041764fb5bd2
JacobJanik <132952597+JacobJanik@users.noreply.github.com>
2024-09-04 19:10:09 +0200

    Add files via upload
    Testowanie

---
+++ View/Child/Child.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zpp_aplikacja.Pages.View.Child
+{
+    public class ChildModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
